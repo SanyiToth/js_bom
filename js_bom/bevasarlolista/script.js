0526ef4f7a99b80e461d5f3afba86b2eec3e9e33
Tóth Sándor <52709221+SanyiToth@users.noreply.github.com>
2021-02-04 15:32:44 +0100

    version1.0

--- js_bom/bevasarlolista/script.js
+++ js_bom/bevasarlolista/script.js
@@ -4,8 +4,6 @@ let clrList = document.getElementById("clearbtn")
 let list = document.getElementById("list1")
 let feedback = document.getElementById("feedback")
 let listArray = getStoredList()
-let clearListItem=document.querySelectorAll(".del")
-
 
 add.addEventListener("click", function (event) {
         event.preventDefault()
@@ -22,10 +20,6 @@ clrList.addEventListener("click", function (event) {
     localStorage.clear()
 })
 
-clearListItem.addEventListener("click", function (event) {
-    localStorage.clear()
-})
-
 
 function getStoredList() {
     if (localStorage.getItem('myitems') !== null) {
@@ -39,13 +33,26 @@ function setStoredList(list) {
     localStorage.setItem('myitems', JSON.stringify(list))
 }
 
+function delItem(event) {
+    const li = event.target.parentElement
+    const id = li.dataset.id
+    const filteredList = getStoredList().filter((item, i) => {
+        return i.toString() !== id
+    })
+    setStoredList(filteredList)
+    listArray = getStoredList()
+    showStoredList()
+}
 
 function showStoredList() {
     list.innerHTML = ""
-    getStoredList().forEach(item => {
-        list.innerHTML += `<li>${item}<button class="del ${getStoredList().indexOf(item)}">X</button></li>`
+    getStoredList().forEach((item, index) => {
+
+        let todo = `<li data-id=${index}>${item}<span class="delbtn" onclick="delItem(event)">X</span></li>`
+        list.innerHTML += todo
     })
 }
+
 
 showStoredList()
 
